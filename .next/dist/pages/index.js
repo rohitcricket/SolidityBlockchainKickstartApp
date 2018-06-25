6d1bcba15ca1473c3b0dc6e004aee30919617251
Rohit Gupta <rohitcricket@gmail.com>
2018-06-24 22:31:05 -0700

    fixed gas bug and started on the React front end

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/rohitgupta/Desktop/Blockchain/kickstart/pages/index.js?entry";
+
+exports.default = function () {
+	return _react2.default.createElement("h1", {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 4
+		}
+	}, "This is the campaign list page!!!");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDcEI7d0JBQU8sY0FBQTs7YUFBQTtlQUFBO0FBQUE7QUFBQSxFQUFBLEVBQVAsQUFBTyxBQUNQO0FBRkQiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL3JvaGl0Z3VwdGEvRGVza3RvcC9CbG9ja2NoYWluL2tpY2tzdGFydCJ9
---
+++ .next/dist/pages/show.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/rohitgupta/Desktop/Blockchain/kickstart/pages/show.js?entry";
+
+exports.default = function () {
+	return _react2.default.createElement("h1", {
+		__source: {
+			fileName: _jsxFileName,
+			lineNumber: 4
+		}
+	}, "This is the show page!!!");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL3Nob3cuanMiXSwibmFtZXMiOlsiUmVhY3QiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU8sQUFFUDs7Ozs7Ozs7a0JBQWUsWUFBTSxBQUNwQjt3QkFBTyxjQUFBOzthQUFBO2VBQUE7QUFBQTtBQUFBLEVBQUEsRUFBUCxBQUFPLEFBQ1A7QUFGRCIsImZpbGUiOiJzaG93LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6Ii9Vc2Vycy9yb2hpdGd1cHRhL0Rlc2t0b3AvQmxvY2tjaGFpbi9raWNrc3RhcnQifQ==
